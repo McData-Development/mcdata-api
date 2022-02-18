3146386ea6764d9793b0dc55e21bd4066415fbaa
Soufiane <soufianesedd@gmail.com>
2022-02-18 15:41:52 +0100

    Stage save

---
+++ src/api/routes/PlayerRoute.ts
@@ -0,0 +1,23 @@
+import Route from './Route';
+
+/**
+ * PlayerRoute
+ * @extends Route
+ */
+class PlayerRoute extends Route {
+
+    constructor() {
+        super();
+        this.init();
+    }
+
+    /**
+     * Initialise routes
+     */
+    private init(): void {
+
+    }
+
+}
+
+export default PlayerRoute;
---
+++ src/api/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+/**
+ * Initialse routes
+ * @param router Express router
+ * @param prefix URL prefix
+ */
+export const initRoutes = (router: Router, prefix: string = '/api'): void => {
+
+    
+
+}
