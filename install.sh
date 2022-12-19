18726e488d688210dc7ed2dba54cc2e11a2d71e6
HalloSouf <soufianesedd@gmail.com>
2022-12-19 19:36:14 +0100

    `Updated` Optimized install bash script

--- install.sh
+++ install.sh
@@ -8,28 +8,8 @@ echo "Initializing project setup"
 echo
 
 echo "(1/3) Installing dependencies"
-manager=none
-which npm &> /dev/null && manager=npm
-which yarn &> /dev/null && manager=yarn
+manager=$(command -v yarn) && yarn install || (rm -f yarn.lock && npm install)
 
-if [ $manager = "yarn" ]
-then
-  echo "Using yarn as package manager."
-  yarn install
-elif [ $manager = "npm" ]
-then
-  echo "Removing yarn.lock if exists"
-  if [ -f "yarn.lock" ]
-  then
-    rm yarn.lock
-  fi
-
-  echo "Using npm as package manager."
-  npm install
-else
-  echo "Could not resolve package manager. Installation will be dismissed."
-  exit
-fi
 
 echo
 echo "(2/3) Creating new .env file from .env.example"
@@ -37,7 +17,7 @@ cp .env.example .env
 
 echo
 echo "(3/3) Generate API key for service"
-[ $manager == "yarn" ] && yarn run scripts:generate-apikey || npm run scripts:generate-apikey
+[ "$manager" = "yarn" ] && yarn run scripts:generate-apikey || npm run scripts:generate-apikey
 
 echo
 echo "Project setup is done!"
